08816459fe70c65671cf5b9665dbea0b92dad04f
Dario Vigil <dariovigil@gmail.com>
2017-10-12 19:48:44 +0200

    Image not available bug corrected

--- index.js
+++ index.js
@@ -4,27 +4,39 @@
 //
 const formInput = document.querySelector('.search-form input');
 const submit = document.querySelector('[type="submit"]');
+loadAllShows();
 
 submit.addEventListener('click', function(ev) {
-  console.log(this);
   console.log(formInput.value);
+  query = `http://api.tvmaze.com/search/shows?q=${formInput.value}`;
   formInput.value = '';
   ev.preventDefault();
+  makeRequest(query);
 });
 //
-// REQUEST API
+// REQUEST
 //
-const req = new Request('http://api.tvmaze.com/shows');
-const shows = fetch(req)
-    .then((function(response) {
-      return(response.json())
-    .then(function(shows) {
-      removeLoading();
-      shows.forEach(show => addShow(show))
-    });
-    }));
+function loadAllShows() {
+  const req = new Request('http://api.tvmaze.com/shows');
+  const shows = fetch(req)
+      .then((function(response) {
+        return(response.json())
+      .then(function(shows) {
+        renderShows(shows);
+      });
+      }));
+}
+
+function makeRequest(query) {
+  const req = new Request(query);
+  const shows = fetch(req)
+      .then((function(response) {
+        return(response.json())
+      .then(shows => renderShows(shows.map(el => el.show)));
+      }));
+}
 //
-// TEMPLATE HTML AND ADD SHOW
+// TEMPLATE HTML AND RENDER SHOWS
 //
 const tvshowsContainer = document.querySelector('.tv-shows');
 const loadingSpinner = document.querySelector('.loading');
@@ -32,13 +44,18 @@ const loadingSpinner = document.querySelector('.loading');
 function removeLoading() {
   loadingSpinner.parentNode.removeChild(loadingSpinner);
 }
-function addShow(show) {
-  let newShow = `<article class="tv-show">
-                    <img src="${show.image.medium}" alt="${show.name} cover">
-                    <div class="info">
-                      <h1>${show.name}</h1>
-                      ${show.summary}
-                    </div>
-                  </article>`
-  tvshowsContainer.innerHTML += newShow;
+
+function renderShows(shows) {
+  // removeLoading();
+  shows.forEach(function(show) {
+    const imageURL = show.image ? show.image.medium : 'https://www.nilfiskcfm.com/wp-content/uploads/2016/12/placeholder.png';
+    let newShow = `<article class="tv-show fadein">
+    <img src="${imageURL}" alt="${show.name} cover">
+    <div class="info">
+    <h1>${show.name}</h1>
+    ${show.summary}
+    </div>
+    </article>`
+    tvshowsContainer.innerHTML += newShow;
+  });
 }
