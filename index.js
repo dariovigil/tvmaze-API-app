6b06e97963a1bcac270b55e8af7968ed3bc43ba4
Dario Vigil <dariovigil@gmail.com>
2017-10-12 11:46:37 +0200

    Added fetch request

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 /*jshint esversion: 6 */
-const header = document.getElementById('app-header');
+//
+// SUBMIT SEARCH FORM
+//
+const formInput = document.querySelector('.search-form input');
+const submit = document.querySelector('[type="submit"]');
+
+submit.addEventListener('click', function(ev) {
+  console.log(this);
+  console.log(formInput.value);
+  formInput.value = '';
+  ev.preventDefault();
+});
+//
+// REQUEST API
+//
+const req = new Request('http://api.tvmaze.com/search/shows?q=girls');
+const myResponse = fetch(req)
+    .then((function(response) {
+      return(response.json())
+    .then(function(json) {console.log(json);});  
+    }));
+
+//console.log(myResponse);
+// console.log(myResponse.PromiseValue);
